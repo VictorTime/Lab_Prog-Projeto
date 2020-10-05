8d948638f3c866fde7d5272f93b1dac04041342c
bannk18 <bannk18@gmail.com>
2020-10-04 21:18:16 -0300

    Atualizacao entidades
    Atualização das classes criadas por Victor

---
+++ Portfolio/src/main/java/com/example/Portifolio/Entidade/curriculo.java
@@ -0,0 +1,35 @@
+package com.example.Portifolio.Entidade;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table (name = "curriculo")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class curriculo {
+    @Id
+    @Column (name = "id_curriculo")
+    private long id_curriculo;
+    @Column (name = "id_atvs")
+    private long id_atvs;
+    @Column (name = "escolaridade")
+    private String escolaridade;
+    @Column (name = "qualificacoes")
+    private String qualificacoes;
+
+    @OneToMany
+    @JoinColumn (name="id_curriculo")
+    private curriculo curriculo;
+}
