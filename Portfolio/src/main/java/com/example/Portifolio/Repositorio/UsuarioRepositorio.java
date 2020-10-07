52240545c317bdaa9e3cef1591e4ca882dde1795
Victor <rogeriovictor@nca.ufma.br>
2020-10-07 20:28:14 -0300

    Implementação do UsuarioService
    Só o espoço da implementação, pois os erros dos ultimos testes ainda não foram concluidos

--- Portfolio/src/main/java/com/example/Portifolio/Repositorio/UsuarioRepositorio.java
+++ Portfolio/src/main/java/com/example/Portifolio/Repositorio/UsuarioRepositorio.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface usuarioRepositorio extends JpaRepository<usuario, Long> {
+
+	boolean existsByEmail(String email);
 }
