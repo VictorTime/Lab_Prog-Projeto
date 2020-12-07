4c4650bd8677a76509ead9eb380c1d8153a1ae77
VictorTime <rogeriovictor@nca.ufma.br>
2020-12-07 14:34:21 -0300

    Mudanças no Controller da atividade

--- src/main/java/com/example/Portifolio/controller/AtividadeController.java
+++ src/main/java/com/example/Portifolio/controller/AtividadeController.java
@@ -7,10 +7,14 @@ import com.example.Portifolio.model.entidade.Atividade;
 import com.example.Portifolio.model.entidade.Usuario;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.ParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +41,39 @@ public class AtividadeController {
             return new ResponseEntity(salvo, HttpStatus.CREATED);
         }
         catch (RegraPortifolioRunTime e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity atualizar(@PathVariable("id") Long idAtividade,
+                                    @RequestBody AtividadeDTO dto) {
+        try {
+            Atividade pos = Atividade.builder()
+                                        .id_atv(idAtividade)
+                                        .tipo_atv(dto.getTipo_atv())
+                                        .link(dto.getLink())
+                                        .titulo(dto.getTitulo())
+                                        .build();
+            Atividade salvo = service.atualizar(pos);
+            return ResponseEntity.ok(salvo);
+        }
+        catch (RegraPortifolioRunTime e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (ParseException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity remover(@PathVariable("id") Long idAtividade) {
+        try {
+            Atividade pos = Atividade.builder().id_atv(idAtividade).build();
+            service.remover(pos);
+            return ResponseEntity.ok(HttpStatus.NO_CONTENT);
+        }
+        catch(RegraPortifolioRunTime e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
